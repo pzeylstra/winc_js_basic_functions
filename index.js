6668dc03f70223b13f2866059035188c7540c73d
Paméla Zeylstra <pamela@pam-organisatieondersteuning.nl>
2021-12-04 15:06:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// writing of 'definining'a funcion
+const maakPizza = function () {
+    console.log("neem 500 gram bloem");
+    console.log("meng bloem met 500 ml handwarm water");
+    console.log("voeg 1 el gist toe");
+    console.log("een snufje zout");
+    console.log("kneed het deeg tot het een soepel geheel is");
+    console.log("laat het een minimaal een uur rijzen");
+    console.log("rol het deeg uit tot een grote lap");
+    console.log("beleg naar smaak met ingrediënten");
+    console.log("bak de pizza zo'n 20 minuten in een oven voorverwarmd op 230 graden")
+};
+
+// using or 'calling' a function
+maakPizza();
+maakPizza();
+
+const maakSushi = function () {
+    console.log("neem een kilo sushi rijst en kook deze gaar");
+    console.log("neem een pak zeewiervellen");
+    console.log("maak gerookte zalm, krab, tonijn, komkommer, gebakken ei klaar voor de vulling");
+    console.log("begin met een vel zeewier, spreid daarop de gekookte rijst uit en leg in het midden komkommer en een van de andere ingrediënten");
+    console.log("rol het geheel strak op en snij in stukken van 3 cm");
+    console.log("serveer er zoute soyasaus, gember en wasabipasta bij");
+};
+
+maakSushi();
+maakSushi();
+maakSushi();
